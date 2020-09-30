f892438e83b910d4fe1358fbfc3d97753aba4dc9
JordanW1313 <jwriker@gmail.com>
2020-09-30 11:07:12 -0400

    Add project files.

---
+++ OOP3200-F2020-ICE3/Student.cpp
@@ -0,0 +1,56 @@
+/**
+ * Project OOP3200-F2020-ICE3
+ * @author Jordan Wriker
+ * @version 1
+ */
+
+
+#include "Student.h"
+
+/**
+ * Student implementation
+ */
+
+
+/**
+ * @param first_name
+ * @param last_name
+ * @param age
+ * @param student_id
+ */
+void Student::Student(string first_name, string last_name, float age, string student_id) {
+
+}
+
+/**
+ * @return string
+ */
+string Student::getStudentID() {
+    return "";
+}
+
+/**
+ * @param value
+ */
+void Student::setStudentID(string value) {
+
+}
+
+void Student::Studies() {
+
+}
+
+
+/**
+ * @return void
+ */
+void Student::Studies() {
+    return;
+}
+
+/**
+ * @return string
+ */
+string Student::ToString() {
+    return "";
+}
